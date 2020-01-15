b069909cbd5abf6b67a922125e3b797220c40eed
Derrick Camerino <robustdj@gmail.com>
2020-01-15 14:52:14 -0800

    Add react app with Registration component

---
+++ ReactAuthApp/src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Registration from './auth/Registration'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Home</h1>
+        <h1>Home</h1>
+        <Registration />
+      </div>
+    )
+  }
+}
