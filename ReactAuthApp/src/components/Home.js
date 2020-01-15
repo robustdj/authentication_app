ad7c1a5cb1b0535252fc69b9f2a0942edac59ed0
Derrick Camerino <robustdj@gmail.com>
2020-01-15 15:48:16 -0800

    Register user and set state to logged in

--- ReactAuthApp/src/components/Home.js
+++ ReactAuthApp/src/components/Home.js
@@ -2,12 +2,23 @@ import React, { Component } from 'react'
 import Registration from './auth/Registration'
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.handleSuccessfulAuth = this.handleSuccessfulAuth.bind(this)
+  }
+
+  handleSuccessfulAuth(data) {
+    this.props.handleLogin(data)
+    this.props.history.push("/dashboard")
+  }
+
   render() {
     return (
       <div className='app'>
         <h1>Home</h1>
-        <h1>Home</h1>
-        <Registration />
+        <h1>Status: {this.props.loggedInStatus}</h1>
+        <Registration handleSuccessfulAuth={this.handleSuccessfulAuth} />
       </div>
     )
   }
