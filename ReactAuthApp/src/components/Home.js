84871c3491acb6935817c34e7c6041ec01ea2df8
Derrick Camerino <robustdj@gmail.com>
2020-01-15 16:03:42 -0800

    Login component

--- ReactAuthApp/src/components/Home.js
+++ ReactAuthApp/src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Registration from './auth/Registration'
+import Login from './auth/Login'
 
 export default class App extends Component {
   constructor(props) {
@@ -19,6 +20,7 @@ export default class App extends Component {
         <h1>Home</h1>
         <h1>Status: {this.props.loggedInStatus}</h1>
         <Registration handleSuccessfulAuth={this.handleSuccessfulAuth} />
+        <Login handleSuccessfulAuth={this.handleSuccessfulAuth} />
       </div>
     )
   }
