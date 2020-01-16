f6c33d38a27c61589a8eb6988a199165c56fb41e
Derrick Camerino <robustdj@gmail.com>
2020-01-15 16:33:05 -0800

    Handle logout

--- ReactAuthApp/src/components/Home.js
+++ ReactAuthApp/src/components/Home.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import Registration from './auth/Registration'
 import Login from './auth/Login'
+import axios from 'axios'
 
 export default class App extends Component {
   constructor(props) {
     super(props)
 
     this.handleSuccessfulAuth = this.handleSuccessfulAuth.bind(this)
+    this.handleLogoutClick = this.handleLogoutClick.bind(this)
   }
 
   handleSuccessfulAuth(data) {
@@ -14,13 +16,32 @@ export default class App extends Component {
     this.props.history.push("/dashboard")
   }
 
+  handleLogoutClick() {
+    axios.delete('http://localhost:3001/logout', { withCredentials: true }).then(response => {
+      this.props.handleLogout()
+    }).catch(error => {
+      console.log("logout error", error)
+    })
+  }
+
   render() {
+    let authComponents
+
+    if (this.props.loggedInStatus === 'NOT_LOGGED_IN') {
+      authComponents = (
+        <div>
+          <Registration handleSuccessfulAuth={this.handleSuccessfulAuth} />
+          <Login handleSuccessfulAuth={this.handleSuccessfulAuth} />
+        </div>
+      )
+    } else {
+      authComponents = <button onClick={() => this.handleLogoutClick()}>Logout</button>
+    }
     return (
       <div className='app'>
         <h1>Home</h1>
         <h1>Status: {this.props.loggedInStatus}</h1>
-        <Registration handleSuccessfulAuth={this.handleSuccessfulAuth} />
-        <Login handleSuccessfulAuth={this.handleSuccessfulAuth} />
+        {authComponents}
       </div>
     )
   }
--- ReactAuthApp/src/components/app.js
+++ ReactAuthApp/src/components/app.js
@@ -2,17 +2,48 @@ import React, { Component } from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Home from './Home'
 import Dashboard from './Dashboard'
+import axios from 'axios'
 
 export default class App extends Component {
   constructor() {
     super()
 
     this.state = {
-      loggedInStatus: "NOT_LOGGED_IN",
+      loggedInStatus: "",
       user: {}
     }
 
     this.handleLogin = this.handleLogin.bind(this)
+    this.handleLogout = this.handleLogout.bind(this)
+  }
+
+  componentDidMount() {
+    this.checkLoginStatus()
+  }
+
+  checkLoginStatus() {
+    axios.get("http://localhost:3001/logged_in", { withCredentials: true }).then(response => {
+      if (response.data.logged_in) {
+        this.setState({
+          loggedInStatus: 'LOGGED_IN',
+          user: response.data.user
+        })
+      } else if (!response.data.logged_in) {
+        this.setState({
+          loggedInStatus: 'NOT_LOGGED_IN',
+          user: {}
+        })
+      }
+    }).catch(error => {
+      console.log("check login error", error)
+    })
+  }
+
+  handleLogout() {
+    this.setState({
+      loggedInStatus: 'NOT_LOGGED_IN',
+      user: {}
+    })
   }
 
   handleLogin(data) {
@@ -31,7 +62,12 @@ export default class App extends Component {
             exact
             path={"/"}
             render={props => (
-              <Home {...props} handleLogin={this.handleLogin} loggedInStatus={this.state.loggedInStatus} />
+              <Home
+                {...props}
+                handleLogin={this.handleLogin}
+                handleLogout={this.handleLogout}
+                loggedInStatus={this.state.loggedInStatus}
+              />
             )}
           />
           <Route
